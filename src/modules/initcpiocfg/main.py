174800b262b7a3a78fad16a2c3b064b035036f03
Philip <philm@manjaro.org>
2017-09-14 08:13:40 -0400

    Revert "[modules] work on #797 and #796"
    This reverts commit 80b1a7e0b642dd27dac614eba94142075b6a83ff.

--- src/modules/initcpiocfg/main.py
+++ src/modules/initcpiocfg/main.py
@@ -114,7 +114,7 @@ def modify_mkinitcpio_conf(partitions, root_mount_point):
         hooks.append("plymouth")
 
     for partition in partitions:
-        if partition["fs"].lower().lstrip("-") == "linuxswap":
+        if partition["fs"].lower() == "linuxswap":
             swap_uuid = partition["uuid"]
             if "luksMapperName" in partition:
                 openswap_hook = True
